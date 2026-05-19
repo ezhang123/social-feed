14d620aae162c15afa42adfb1d33211527aacf55
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2026-05-18 22:08:14 -0400

    tested api requests with postman

--- src/main/java/com/eric/socialfeed/controller/PostController.java
+++ src/main/java/com/eric/socialfeed/controller/PostController.java
@@ -2,26 +2,39 @@ package com.eric.socialfeed.controller;
 
 import com.eric.socialfeed.model.Post;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public List<Post> getPosts() {
+    private List<Post> posts = new ArrayList<>();
 
+    public PostController() {
         Post post = new Post();
 
+        //for "fake" initial data to test
         post.setId(1);
-        post.setContent("Testing Post");
+        post.setContent("Testing Post 2");
 
-        List<Post> posts = new ArrayList<>();
+        posts.add(post);
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(@RequestBody Post post){
         posts.add(post);
 
-        return posts;
+        return post;
     }
 
 }
