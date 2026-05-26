76984e6810540c896421b8bcdb8c805c2ff1e92d
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2026-05-25 21:23:25 -0400

    added update and delete. to posts. now it's full CRUD. need to work on sorting the database in asc next.

--- src/main/java/com/eric/socialfeed/controller/PostController.java
+++ src/main/java/com/eric/socialfeed/controller/PostController.java
@@ -2,10 +2,8 @@ package com.eric.socialfeed.controller;
 
 import com.eric.socialfeed.model.Post;
 import com.eric.socialfeed.service.PostService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.PostUpdate;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +26,16 @@ public class PostController {
     @PostMapping("/posts")
     public Post createPost(@RequestBody Post post){
         return postService.createPost(post);
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post updatePost(@PathVariable int id, @RequestBody Post updatePost){
+        return postService.updatePost(id, updatePost);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deletePost(@PathVariable int id) {
+        postService.deletePost(id);
     }
 
 }
--- src/main/java/com/eric/socialfeed/service/PostService.java
+++ src/main/java/com/eric/socialfeed/service/PostService.java
@@ -2,15 +2,17 @@ package com.eric.socialfeed.service;
 
 import com.eric.socialfeed.model.Post;
 import com.eric.socialfeed.repository.PostRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
 
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public PostService(PostRepository postRepository){
         this.postRepository = postRepository;
@@ -24,4 +26,26 @@ public class PostService {
         return postRepository.save(post);
     }
 
+
+    public void deletePost(int id){
+
+        if(postRepository.existsById(id)) {
+            postRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Post not found"
+            );
+        }
+    }
+
+    public Post updatePost(int id, Post updatePost) {
+
+        Post existingPost = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+
+        existingPost.setContent(updatePost.getContent());
+
+        return postRepository.save(existingPost);
+    }
 }
